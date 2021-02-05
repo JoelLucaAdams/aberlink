42b017682b04a4525f12f59be20b79451b357fd2
joa38 <joa38@aber.ac.uk>
2021-02-05 18:45:49 +0000

    Added JSON response option

--- src/AberLinkAuthentication/login/views.py
+++ src/AberLinkAuthentication/login/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
+import json
 
 def pancakes(request):
-    return HttpResponse("HAHA, it worked! Hello " + str(request.META['OIDC_CLAIM_preferred_username']))
-
+    return HttpResponse(str(request.META))
+    ''' # example of Json response usage
+    metadata = request.META
+    return JsonResponse({
+        'username': metadata['OIDC_CLAIM_preferred_username'],
+        'name': metadata['OIDC_CLAIM_name'],
+        'family name': metadata['OIDC_CLAIM_family_name'],\
+    })
+    '''
