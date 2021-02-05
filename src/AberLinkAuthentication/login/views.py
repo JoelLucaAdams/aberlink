d6b6e6f3de67844f2ef2d98f943c776b2f640470
joa38 <joa38@aber.ac.uk>
2021-02-05 17:23:24 +0000

    Added simple metadata response

--- src/AberLinkAuthentication/login/views.py
+++ src/AberLinkAuthentication/login/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-@login_required
-def test(request):
-    return HttpResponse("HAHA, it worked!." + str(request.user))
+def pancakes(request):
+    return HttpResponse("HAHA, it worked! Hello " + str(request.META['OIDC_CLAIM_preferred_username']))
+
