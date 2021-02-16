4f82796468385f1182aec1158330d89f168a2eb0
joa38 <joa38@aber.ac.uk>
2021-02-16 20:36:46 +0000

    Added new models to admin page

--- src/AberLinkAuthentication/login/admin.py
+++ src/AberLinkAuthentication/login/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import OpenIDCUser, DiscordUser
+from .models import OpenIDCUser, DiscordUser, Staff, Undergrad
 # Register your models here.
 
 admin.site.register(OpenIDCUser)
 admin.site.register(DiscordUser)
+admin.site.register(Staff)
+admin.site.register(Undergrad)
