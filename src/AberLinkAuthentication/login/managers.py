1cd7fa9adb6fd8cf0d01a0e38c71d3ea3142df3d
joa38 <joa38@aber.ac.uk>
2021-02-17 18:11:30 +0000

    removed old openidc manager

--- src/AberLinkAuthentication/login/managers.py
+++ src/AberLinkAuthentication/login/managers.py
@@ -2,23 +2,12 @@ from django.contrib.auth import models
 
 class DiscordUserOAuth2Manager(models.UserManager):
 
-    def create_new_discord_user(self, user, openidc_user):
+    def create_user(self, user, openidc_user):
         discord_username = '%s#%s' % (user['username'], user['discriminator']) 
         new_user = self.create(
             id=user['id'],
             username=discord_username,
             openidc=openidc_user
-        )
-        return new_user
-
-class OpenIDCUserManager(models.UserManager):
-
-    def create_new_openidc_user(self, user):
-        new_user = self.create(
-            username = user['OIDC_CLAIM_preferred_username'],
-            name = user['OIDC_CLAIM_name'],
-            email = user['OIDC_CLAIM_email'],
-            usertype = user['OIDC_CLAIM_usertype']
         )
         return new_user
         
