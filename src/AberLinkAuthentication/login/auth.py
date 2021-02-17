2f73753892fd4e4e1eb684d71c1a409d9f9675bf
joa38 <joa38@aber.ac.uk>
2021-02-17 18:14:07 +0000

    refactored calls to create new objects

--- src/AberLinkAuthentication/login/auth.py
+++ src/AberLinkAuthentication/login/auth.py
@@ -7,7 +7,7 @@ class DiscordAuthenticationBackend(BaseBackend):
         find_user = DiscordUser.objects.filter(id=user['id'])
         if len(find_user) == 0:
             print("User not in database... adding Discord user")
-            new_user = DiscordUser.objects.create_new_discord_user(user, openidc_user)
+            new_user = DiscordUser.objects.create_user(user, openidc_user)
             return DiscordUser.objects.filter(id=user['id'])
         return find_user
 
@@ -23,7 +23,7 @@ class OpenIDCAuthenticationBackend(BaseBackend):
         find_user = OpenIDCUser.objects.filter(username=user['OIDC_CLAIM_preferred_username'])
         if len(find_user) == 0:
             print("User not in database... adding OpenIDC user")
-            new_user = OpenIDCUser.objects.create_new_openidc_user(user)
+            new_user = OpenIDCUser.objects.create_user(user)
             return OpenIDCUser.objects.filter(username=user['OIDC_CLAIM_preferred_username'])
         return find_user
 
