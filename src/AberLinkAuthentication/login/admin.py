d3fecb672c3301a0e40f7aa71e7c3fa8328979a7
joa38 <joa38@aber.ac.uk>
2021-02-17 18:15:41 +0000

    Changed admin pages to display information on openidc and discord users

--- src/AberLinkAuthentication/login/admin.py
+++ src/AberLinkAuthentication/login/admin.py
@@ -1,8 +1,70 @@
 from django.contrib import admin
-from .models import OpenIDCUser, DiscordUser, Staff, Undergrad
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+from django import forms
+from .models import OpenIDCUser, DiscordUser
 # Register your models here.
 
-admin.site.register(OpenIDCUser)
-admin.site.register(DiscordUser)
-admin.site.register(Staff)
-admin.site.register(Undergrad)
+class DiscordUserChangeForm(forms.ModelForm):
+    class Meta:
+        model = DiscordUser
+        fields = ('id', 'username', 'openidc')
+
+class DiscordAdmin(UserAdmin):
+    form = DiscordUserChangeForm
+
+    list_display = ('id', 'username', 'openidc')
+    list_filter = ('openidc',)
+    fieldsets = (
+        (None, {'fields': ('id', 'username', 'openidc')}),
+    )
+    readonly_fields = ('id', 'username', 'openidc')
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide'),
+            'fields': ('id', 'username', 'openidc')
+        })
+    )
+    search_fields = ('id', 'username', 'openidc',)
+    ordering = ('id', 'username', 'openidc')
+    filter_horizontal = ()
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = OpenIDCUser
+        fields = ('id', 'username', 'name', 'email', 'usertype', 'is_admin')
+
+class UserAdmin(UserAdmin):
+    form = UserChangeForm
+
+    list_display = ('username', 'name', 'email', 'usertype', 'is_admin')
+    list_filter = ('usertype',)
+    fieldsets = (
+        (None, {'fields': ('username', 'name', 'email', 'usertype')}),
+        #('Personal info', (None))),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    readonly_fields = ('username', 'name', 'email', 'usertype')
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide'),
+            'fields': ('username', 'name', 'email', 'usertype')
+        })
+    )
+    search_fields = ('email', 'name',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+admin.site.register(OpenIDCUser, UserAdmin)
+admin.site.register(DiscordUser, DiscordAdmin)
+
+admin.site.unregister(Group)
