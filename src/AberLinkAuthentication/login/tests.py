ea9f5d19734b757c1a5c0202ab42106c548f55e3
joa38 <joa38@aber.ac.uk>
2021-03-18 20:05:42 +0000

    Added more tests for Django

--- src/AberLinkAuthentication/login/tests.py
+++ src/AberLinkAuthentication/login/tests.py
@@ -1,5 +1,6 @@
 from django.test.testcases import SimpleTestCase, TestCase
 from django.urls import resolve, reverse
+from django.contrib import admin
 
 from login import views
 from login.models import OpenIDCUser, DiscordUser
@@ -8,7 +9,7 @@ from login.models import OpenIDCUser, DiscordUser
 class TestUrls(SimpleTestCase):
 
     def test_url_openidc(self):
-        url = reverse('OpenID-Connect-response')
+        url = reverse('Home')
         self.assertEquals(resolve(url).func, views.openidc_response)
 
     def test_url_discord_oauth(self):
@@ -20,8 +21,24 @@ class TestUrls(SimpleTestCase):
         self.assertEquals(resolve(url).func, views.discord_oauth2_redirect)
 
     def test_url_logged_in(self):
-        url = reverse('logged-in')
+        url = reverse('logged-in-accounts')
         self.assertEquals(resolve(url).func, views.get_authenticated_user)
+
+    def test_url_deleted_user(self):
+        url = reverse('User-data-deleted')
+        self.assertEquals(resolve(url).func, views.deleted_user)
+
+    def test_url_privacy_policy(self):
+        url = reverse('privacy-policy')
+        self.assertEquals(resolve(url).func, views.privacy_policy_view)
+
+    def test_url_major_project(self):
+        url = reverse('major-project')
+        self.assertEquals(resolve(url).func, views.about_major_project_view)
+
+    def test_url_admin(self):
+        url = reverse('Admin')
+        self.assertEquals(resolve(url).func, admin.site.urls)
 
 class TestModels(TestCase):
 
@@ -44,3 +61,36 @@ class TestModels(TestCase):
         )
         self.assertEquals(self.discord_user1.id, 12213123123)
         self.assertEquals(self.discord_user1.openidc, self.openidc_user1)
+
+    def test_model_discord_user_add_two(self):
+        self.openidc_user1 = OpenIDCUser.objects.create(
+            username="abc123",
+            name="Bob Ross",
+            email="abc123@aber.ac.uk",
+            usertype="undergrad"
+        )
+        self.discord_user1 = DiscordUser.objects.create(
+            id=12213123123,
+            openidc=self.discord_user1
+        )
+        self.discord_user2 = DiscordUser.objects.create(
+            id=78342545464,
+            openidc=self.discord_user1
+        )
+        self.assertEquals(self.discord_user1.id, 12213123123)
+        self.assertEquals(self.discord_user1.openidc, self.openidc_user1)
+        self.assertEquals(self.discord_user2.id, 78342545464)
+        self.assertEquals(self.discord_user1.openidc, self.openidc_user1)
+
+    def test_model_openidc_user_staff(self):
+        self.openidc_user2 = OpenIDCUser.objects.create(
+            username="abc123",
+            name="Bob Ross",
+            email="abc123@aber.ac.uk",
+            usertype="staff"
+        )
+        self.assertEquals(self.openidc_user2.username, "abc123")
+        self.assertEquals(self.openidc_user2.name, "Bob Ross")
+        self.assertEquals(self.openidc_user2.email, "abc123@aber.ac.uk")
+        self.assertEquals(self.openidc_user2.usertype, "undergrad")
+        self.assertEquals(self.openidc_user2.is_admin, True)
