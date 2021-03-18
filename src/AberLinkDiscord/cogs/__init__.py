472f82c3a2eaa1da6efea71d71455dd7bd7671d6
joa38 <joa38@aber.ac.uk>
2021-03-18 14:17:21 +0000

    Added more discord emojis and some formatting

--- src/AberLinkDiscord/cogs/__init__.py
+++ src/AberLinkDiscord/cogs/__init__.py
@@ -1,7 +1,15 @@
 admin_roles = ['Admin role', 'ADMIN ROLE', 'Admin', 'Devs', 'Dev', 'lecturer', 'LECTURER']
 
 # use \:emote: to get
-emojis = {'aberlink': '<:AberLink:813468686647033936>', 'discord': '<:discord:814098231142514697>', 'aberlink_error': '<:AberLink_oops:816019518681776157>', 'aberlink_database': '<:AberLink_database:820659743353470998>'}
+emojis = {
+    'aberlink': '<:AberLink:813468686647033936>', 
+    'discord': '<:discord:814098231142514697>', 
+    'aberlink_error': '<:AberLink_oops:816019518681776157>', 
+    'aberlink_database': '<:AberLink_database:820659743353470998>',
+    'demohelper': '<:demohelper:822070615023681567>',
+    'muddy_points': '<:muddy_points:822073052115370055>',
+    'simple_poll': '<:simple_poll:822074760686600213>'
+    }
 
 # test server guild
 guild_ids = [802212304216260661]
